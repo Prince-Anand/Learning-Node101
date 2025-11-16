95e46d361af0c6b52b228fc395bf54be106247a3
Prince Anand <pk933466@gmail.com>
2025-11-16 08:22:06 +0530

    notes

--- 02 - L4/xyz.js
+++ 02 - L4/xyz.js
@@ -1,1 +1,13 @@
 console.log("This is the xyz.js very important file.");
+
+// All module code is wrapped in a function by Node.js
+
+// IIFE - Immediately Invoked Function Expression
+//  IIFE immediately invokes the function right after defining it.
+// It keeps the variables and functions defined inside the IIFE private and keeps it safe
+// (function (){})(); - This is the syntax of IIFE
+
+
+(function (){
+    // All module code is written here
+})();
