e6c5fdf1de6c48230c6c2efb6ee686f4a3cbcf8c
Prince Anand <pk933466@gmail.com>
2025-11-29 23:48:15 +0530

    Websocket DAMN ;-)

---
+++ Practice for exam/websocket.js
@@ -0,0 +1,13 @@
+// this can be useful for testing purposes.
+
+// Websocket . io provides a simple and efficient way to implement real-time, bidirectional communication between clients and servers using WebSockets. It is widely used in applications such as chat apps, live notifications, and online gaming.
+
+// we can make real time typing speed test application,  for multiple user race
+// also scribble like application can be made using websocket.
+
+// To get started with WebSocket . io, you can install it via npm: 
+// npm install socket.io
+// Once installed, you can set up a basic WebSocket server and client as follows:
+// // Server-side (Node.js)
+// const http = require('http');
+// const socketIo = require('socket.io');
