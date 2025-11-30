7fd803d46596a842de9c3c5f437c719be8af3fca
Prince Anand <pk933466@gmail.com>
2025-11-30 22:02:15 +0530

    Server

---
+++ 04 - Server/index.js
@@ -0,0 +1,9 @@
+// Server - It is a computer which handles client requests . 
+// Server can be hardware or software
+// Server has an application to handle requests
+
+
+// Client and Server are connected through socket connection
+// TCP/IP protocol are rules they follow to send data
+// HttpProxy,smtp,ftp, etc are the language in which they communicate
+
